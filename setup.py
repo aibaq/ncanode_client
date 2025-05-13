ab6a14453de7a7f0dcc49aa26f6fad29db6a673e
Aibek <aiba.prenov@gmail.com>
2025-05-13 14:15:20 +0500

    bump setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ classifier_list = [c for c in classifiers.split("\n") if c]
 
 setup(
     name="ncanode_client",
-    version="1.0.4",
+    version="1.0.5",
     author="Aibek Prenov",
     description="NCANode python client",
     url="https://github.com/aibaq/ncanode_client",
